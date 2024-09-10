7712c7ede06039c2356f8d5f66b94aa33e130a73
23DP2ERemi <166109565+23DP2ERemi@users.noreply.github.com>
2024-09-10 09:37:34 +0000

    temp

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,40 +6,38 @@ public class App
 {
     public static void main(String[] args) {
 
-        // Scanner scanner = new Scanner(System.in);
 
-        // System.out.println("Write a message: ");
+        // c
 
-        // String message = scanner.nextLine();
+        // System.out.println("enter your name:");
 
-        // System.out.println(message + " - your message");
+        // String name = scanner.nextLine();
 
-        // String name = "Mark";
-        // int age = 17;
 
-        // System.out.println("hello, " + name + " your age is - " + age);
+
+        // System.out.println("enter your surname:");
+
+        // String surname = scanner.nextLine();
+
+
+        // System.out.println("enter your group:");
+
+        // String group = scanner.nextLine();
+
+
+        // System.out.println("your name is: " + name + ", your surname is: " + surname + " and your group is - " + group);
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("enter your name:");
+        int num1 = Integer.valueOf(scanner.nextLine());
 
-        String name = scanner.nextLine();
+        int num1 = Integer.valueOf(scanner.nextLine());
+        
+        int sum = num1 + num2;
 
-
-
-        System.out.println("enter your surname:");
-
-        String surname = scanner.nextLine();
-
-
-        System.out.println("enter your group:");
-
-        String group = scanner.nextLine();
-
-
-        System.out.println("your name is: " + name + ", your surname is: " + surname + " and your group is - " + group);
+        System.out.println("sum is: " + sum);
     }
 
-    
+
 
 }
