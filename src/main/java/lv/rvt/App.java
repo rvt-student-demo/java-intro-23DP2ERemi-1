dacfcff9c831c6053b84469be2e1f77561725d8d
23DP2ERemi <166109565+23DP2ERemi@users.noreply.github.com>
2024-09-10 09:13:26 +0000

    uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,20 +1,44 @@
 package lv.rvt;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.util.Scanner;
 
 public class App 
 {
     public static void main(String[] args) {
 
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("Write a message: ");
+
+        // String message = scanner.nextLine();
+
+        // System.out.println(message + " - your message");
+
+        // String name = "Mark";
+        // int age = 17;
+
+        // System.out.println("hello, " + name + " your age is - " + age);
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write a message: ");
+        System.out.println("enter your name:");
 
-        String message = scanner.nextLine();
+        String name = scanner.nextLine();
 
-        System.out.println(message);
 
+
+        System.out.println("enter your surname:");
+
+        String surname = scanner.nextLine();
+
+
+        System.out.println("enter your group:");
+
+        String group = scanner.nextLine();
+
+
+        System.out.println("your name is: " + name + ", your surname is: " + surname + " and your group is - " + group);
     }
+
+
 }
